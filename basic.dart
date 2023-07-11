163c67dd39f9e898ffff4e18d7a30b7f358500c2
aditya2kumar465625 <adity2kumar465625@gmail.com>
2023-07-12 00:30:14 +0530

    first commit

---
+++ basic.dart
@@ -0,0 +1,53 @@
+main() {
+  print("hello world");
+  //data types in dart
+  /*
+    * int
+    * double
+    * boolean
+    * String
+    * map
+    * list
+    * dynamic
+    * */
+//   int a = 5;
+//    String str = "Aditya ";
+//    double b = 4.3;
+//    var c = true;
+//    List d = [1,2,3,4];
+//    Map e = {"name" : "aditya","surname" :"kumar"};
+//    print("$a, $str , $b, $c , $d , $e");
+//        String function in dart
+//    var str = " AdityA ";
+//    str = str.trim();       use for remove string spaces
+  //    var l = str.length;
+//    print(l);
+  //string compare
+//    var str = 'aditya';
+//    var str2 = 'Aditya';
+//    print(str.compareTo(str2));
+
+//    var str = 'This is aditya kumar';
+//    print(str.replaceAll("aditya","Aditya"));
+//    var a = str.split(' ');
+//    print(a);
+//    var str2 = " this is aditya , this is prashant ";
+//    var b = str2.split(',');
+//    print(b);
+//    var check = " aditya ";
+//    print(check.toUpperCase()..trim());
+//    print(check is String);
+//   print(sum(3, 4));
+//   sum2(5);
+// sum(3,z:5,y:8);
+
+}
+
+// sum(int a, int b) => a + b;
+
+// sum2(int a, [int b = 56,int c = 543]) {
+//   print("$a,$b");
+//}
+// sum(int x,{int y = 0, int z = 0}){
+// print("$x , $y ,$z");
+//}
